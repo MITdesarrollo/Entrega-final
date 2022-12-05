c1dfbfca4fb95d4030f3c92936f360dab0df2921
Mariel Torres <marieltorres97@gmail.com>
2022-12-05 14:25:26 -0300

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import {PaginaNoEncontradaComponent} from "./core/components/pagina-no-encontrad
 import {LoginComponent} from "./login/components/login/login.component";
 import {AutenticacionGuard} from "./core/guards/autenticacion.guard";
 import { RegisterComponent } from './login/components/register/register.component';
+import { AdminGuard } from './core/guards/admin.guard';
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path:'login', component: LoginComponent},
-  {path:'courses', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule),  },
-  {path:'students', loadChildren: () => import('./alumnos/alumnos.module').then((m => m.AlumnosModule)),canActivate: [AutenticacionGuard] },
+  {path:'courses', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule),canActivate: [AutenticacionGuard]  },
+  {path:'students', loadChildren: () => import('./alumnos/alumnos.module').then((m => m.AlumnosModule)),canActivate: [AutenticacionGuard, AdminGuard] },
   {path:'home', component: HomeComponent, canActivate: [AutenticacionGuard]},
   {path: '', redirectTo:'home', pathMatch:'full'},
   {path:'**', component: PaginaNoEncontradaComponent}
